c461df7c7d6e333acb2f664fb1eff00fd17c8c3c
userNims <86461767+userNims@users.noreply.github.com>
2022-03-13 22:48:22 -0300

    Desafio 04 - Js

--- script.js
+++ script.js
@@ -181,3 +181,26 @@ function desafio_03(){
 
 }
 
+
+
+// ---------------- DESAFIO 04 ----------------
+  // DOM elements
+  const gritterForm = document.getElementById('gritter-form');
+
+  function addGreet(event) {
+      event.preventDefault();
+      const greet = gritterForm.greet.value;
+      const newGreetLi = document.createElement('li');
+      const newGreetAvatar = document.createElement('div');
+      newGreetAvatar.className = "avatar";
+      const newGreetText = document.createElement('span');
+      newGreetText.innerText = greet;
+      const greets = document.getElementById('greets').getElementsByTagName('ul')[0];
+      const newerGreetLi = greets.appendChild(newGreetLi);
+      newerGreetLi.appendChild(newGreetAvatar);
+      newerGreetLi.appendChild(newGreetText);
+      gritterForm.greet.value = "";
+  }
+
+// add event handlers when mouse events are triggered
+gritterForm.onsubmit = addGreet;
